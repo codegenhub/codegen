d9b901cca5d60d7e2feca67414e4e58d657c7ed6
ivankuznetsov <kuznecov.fd0@yandex.ru>
2023-08-09 22:51:28 +0300

    Installation fixes.

--- install-codegen.php
+++ install-codegen.php
@@ -6,25 +6,48 @@ $packageName = 'codegenhub/codegen';
 // Determine the OS
 $os = strtoupper(substr(PHP_OS, 0, 3));
 
+// Check if global composer is available
+$composerCommand = 'composer';
+if (trim(shell_exec('which composer')) == '') {
+    echo "Global Composer not found. Downloading composer.phar...\n";
+    file_put_contents('composer.phar', fopen('https://getcomposer.org/composer-stable.phar', 'r'));
+    $composerCommand = 'php composer.phar';
+}
+
 // Install the package globally using Composer
 echo "Installing $packageName via Composer...\n";
-shell_exec('composer global require ' . $packageName);
+shell_exec($composerCommand . ' global require ' . $packageName);
 
 // Determine the path for the global Composer bin directory
-$composerBinDir = $os === 'WIN'
-    ? shell_exec('composer global config bin-dir --absolute')
-    : rtrim(shell_exec('composer global config bin-dir --absolute'), "\n");
+$composerBinDir = rtrim(shell_exec($composerCommand . ' global config bin-dir --absolute'), "\n");
 
 // Symlink or copy the codegen binary to a directory in the user's PATH
 $binaryPath = $composerBinDir . '/codegen';
 $destPath = $os === 'WIN' ? 'C:/Windows/System32/codegen' : '/usr/local/bin/codegen';
 
+if (file_exists($destPath)) {
+    echo "Removing existing codegen command...\n";
+    unlink($destPath);
+}
+
 if ($os !== 'WIN') {
     echo "Creating symlink to codegen...\n";
-    symlink($binaryPath, $destPath);
+    if (!symlink($binaryPath, $destPath)) {
+        echo "Failed to create symlink. Please ensure you have the necessary permissions.\n";
+        exit(1);
+    }
+    chmod($destPath, 0755);
 } else {
     echo "Copying codegen binary...\n";
-    copy($binaryPath, $destPath);
+    if (!copy($binaryPath, $destPath)) {
+        echo "Failed to copy binary. Please ensure you have the necessary permissions.\n";
+        exit(1);
+    }
+}
+
+// Remove local composer.phar if it was downloaded
+if ($composerCommand === 'php composer.phar') {
+    unlink('composer.phar');
 }
 
 echo "Installation successful! You can now use the 'codegen' command.\n";
