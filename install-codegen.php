b491229b77c1f7f9285a5ab289f70660fbbd219c
ivankuznetsov <kuznecov.fd0@yandex.ru>
2023-08-09 23:03:23 +0300

    Added green ticks + installation logic changes.

--- install-codegen.php
+++ install-codegen.php
@@ -13,11 +13,13 @@ if (!$whichComposer || trim($whichComposer) == '') {
     echo "Global Composer not found. Downloading composer.phar...\n";
     file_put_contents('composer.phar', fopen('https://getcomposer.org/composer-stable.phar', 'r'));
     $composerCommand = 'php composer.phar';
+    echo "\e[32m✓\e[0m Downloaded composer.phar\n";
 }
 
 // Install the package globally using Composer with --no-plugins flag
 echo "Installing $packageName via Composer...\n";
 shell_exec($composerCommand . ' global require ' . $packageName . ' --no-plugins');
+echo "\e[32m✓\e[0m Installed $packageName\n";
 
 // Determine the path for the global Composer bin directory
 $composerBinDirOutput = shell_exec($composerCommand . ' global config bin-dir --absolute');
@@ -42,6 +44,11 @@ if (!file_exists($binaryPath)) {
 // Symlink or copy the codegen binary to a directory in the user's PATH
 $destPath = $os === 'WIN' ? 'C:/Windows/System32/codegen' : getenv('HOME') . '/.local/bin/codegen';
 
+// Ensure the target directory exists (for UNIX systems)
+if ($os !== 'WIN' && !is_dir(dirname($destPath))) {
+    mkdir(dirname($destPath), 0755, true);
+}
+
 if (file_exists($destPath)) {
     echo "Removing existing codegen command...\n";
     unlink($destPath);
@@ -54,18 +61,21 @@ if ($os !== 'WIN') {
         exit(1);
     }
     chmod($destPath, 0755);
+    echo "\e[32m✓\e[0m Created symlink to codegen\n";
 } else {
     echo "Copying codegen binary...\n";
     if (!copy($binaryPath, $destPath)) {
         echo "Failed to copy binary. Please ensure you have the necessary permissions.\n";
         exit(1);
     }
+    echo "\e[32m✓\e[0m Copied codegen binary\n";
 }
 
 // Remove local composer.phar if it was downloaded
 if ($composerCommand === 'php composer.phar') {
     unlink('composer.phar');
+    echo "\e[32m✓\e[0m Removed local composer.phar\n";
 }
 
-echo "Installation successful! You can now use the 'codegen' command.\n";
+echo "\e[32m✓\e[0m Installation successful! You can now use the 'codegen' command.\n";
 echo "Ensure that ~/.local/bin is in your PATH.\n";
